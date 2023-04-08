37363f39078e6c7480c4d5af2915ea253a07a321
matyalatte <matyalatte@gmail.com>
2023-04-09 01:07:08 +0900

    enable skip_non_texture option and cubic filter as default

--- bat/_3_inject.bat
+++ bat/_3_inject.bat
@@ -5,7 +5,7 @@ REM You can specify the asset path with _export_as_tga.bat or _set_asset_path.ba
 @if "%~1"=="" goto skip
 
 @pushd %~dp0
-python\python.exe src\main.py src\_file_path_.txt "%~1" --save_folder=injected
+python\python.exe src\main.py src\_file_path_.txt "%~1" --save_folder=injected --skip_non_texture --image_filter=cubic
 @popd
 
 pause
