9fbdd29f22f5429616ef9784494c9034b0643a68
matyalatte <matyalatte@gmail.com>
2024-05-04 17:53:28 +0900

    fix errors when reading some ucas/utoc assets for UE5.4

--- src/unreal/utexture.py
+++ src/unreal/utexture.py
@@ -122,12 +122,6 @@ class Utexture:
         start_offset = ar.tell()
         err_offset = min(ar.size - 7, start_offset + 1000)
 
-        if ar.version <= "5.3":
-            search_bin = b"\x01\x00\x01\x00\x01\x00\x00\x00"
-        else:
-            # The default value of StripFlags is five from UE5.4
-            search_bin = b"\x05\x00\x05\x00\x01\x00\x00\x00"
-
         while (True):
             """ Search and skip to \x01\x00\x01\x00\x01\x00\x00\x00.
             \x01\x00 is StripFlags for UTexture
@@ -137,12 +131,18 @@ class Utexture:
             Just searching x01 is not the best algorithm but fast enough.
             Because "found 01" means "found strip flags" for most texture assets.
             """
-            while (ar.read(1)[0] != search_bin[0]):
+            b = ar.read(1)
+            while (b != b"\x01" and b != b"\x05"):
                 if (ar.tell() >= err_offset):
                     ar.raise_error()
+                b = ar.read(1)
 
-            if ar.read(7) == search_bin[1:]:
-                # Found search_bin
+            b2 = ar.read(7)
+            if b == b"\x01" and b2 == b"\x00\x01\x00\x01\x00\x00\x00":
+                # Found
+                break
+            elif b == b"\x05" and b2 == b"\x00\x05\x00\x01\x00\x00\x00":
+                # The default value of StripFlags is five from UE5.4
                 break
             else:
                 ar.seek(-7, 1)
